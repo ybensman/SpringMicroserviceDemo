9675181004a57f66f7f103eb20306efa619119a1
Bensman, Yulia <yb444b@intl.att.com>
2023-06-13 23:52:09 +0300

    Revert "Solutions for configuration and validation"
    This reverts commit 93e4ecf9b3e893e0f6971be0812db474f21e9609.

--- src/main/java/com/example/demo/model/UniversalCalculateRequest.java
+++ src/main/java/com/example/demo/model/UniversalCalculateRequest.java
@@ -1,18 +1,6 @@
 package com.example.demo.model;
 
-import javax.validation.constraints.*;
-
 public record UniversalCalculateRequest(
-        @NotNull
-        @NotBlank
-        @Min(1)
-        @Max(100)
-        @Pattern(regexp = "^[0-9]*[a-f]*$")
-        String num1,
-        @NotNull
-        @NotBlank
-        @Min(1)
-        @Max(100)
-        @Pattern(regexp = "^[0-9]*[a-f]*$")
-        String num2) {
+    String num1,
+    String num2) {
 }
--- src/test/java/com/example/demo/CalculateControllerTest.java
+++ src/test/java/com/example/demo/CalculateControllerTest.java
@@ -1,26 +1,25 @@
 package com.example.demo;
 
 import com.example.demo.controller.CalculateController;
-import com.example.demo.service.CalculationService;
 import com.example.demo.service.CalculationServiceFactory;
 import com.example.demo.service.PrintConfigService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CalculateController.class)
-//@Import(CalculationServiceFactory.class)
+@Import(CalculationServiceFactory.class)
 class CalculateControllerTest {
     @Autowired
     MockMvc mockMvc;
@@ -31,102 +30,20 @@ class CalculateControllerTest {
     @MockBean
     PrintConfigService printConfigService;
 
-    @MockBean
-    @Qualifier("decCalculationService")
-    CalculationService decCalculationService;
-
-    @MockBean
-    @Qualifier("hexCalculationService")
-    CalculationService hexCalculationService;
-
     @Test
-    void testAdd_whenValidInput_thenReturnCorrectResult() throws Exception {
+    void whenValidInput_thenReturnCorrectResult() throws Exception {
         int num1 = 1, num2 = 2;
+        String body = """
+                        {
+                            "num1": %d,
+                            "num2": %d
+                        }
+                        """.formatted(num1, num2);
 
-        mockMvc.perform(get("/calculate/add/{num1}/{num2}", num1, num2))
+        mockMvc.perform(post("/calculate/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body))
                 .andExpect(status().isOk())
-                .andExpect(content().string((String.valueOf(num1 + num2))));
-    }
-
-    @Test
-    void testAdd_whenInvalidInput_thenReturnBadRequest() throws Exception {
-        int num1 = 1, num2 = 102;
-
-        mockMvc.perform(get("/calculate/add/{num1}/{num2}", num1, num2))
-                .andExpect(status().isBadRequest())
-                .andExpect(content().string("Input validation failed: add1.num2: must be less than or equal to 100"));
-    }
-
-    @Test
-    void testUniversalAdd_whenValidInput_thenReturnCorrectResult() throws Exception {
-        int num1 = 1, num2 = 2;
-        String body = """
-                        {
-                            "num1": %d,
-                            "num2": %d
-                        }
-                        """.formatted(num1, num2);
-
-        mockMvc.perform(post("/calculate/universalAdd")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .param("numeralSystem","DEC")
-                        .content(body))
-                .andExpect(status().isOk());
-                //.andExpect(jsonPath("$.result", is(String.valueOf(num1 + num2))));
-    }
-
-    @Test
-    void testUniversalAdd_whenMissingRequestParam_returnBadRequest() throws Exception {
-        int num1 = 1, num2 = 10;
-        String body = """
-                        {
-                            "num1": %d,
-                            "num2": %d
-                        }
-                        """.formatted(num1, num2);
-
-        mockMvc.perform(post("/calculate/universalAdd")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(body))
-                .andExpect(status().isBadRequest())
-        ;
-    }
-
-    @Test
-    void testUniversalAdd_whenInvalidRequestParam_thenReturnBadRequest() throws Exception {
-        int num1 = 1, num2 = 10;
-        String body = """
-                        {
-                            "num1": %d,
-                            "num2": %d
-                        }
-                        """.formatted(num1, num2);
-
-        mockMvc.perform(post("/calculate/universalAdd")
-                        .param("numeralSystem", "OCT")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(body))
-                .andExpect(status().isBadRequest())
-                .andExpect(content().string("Illegal Access!"))
-        ;
-    }
-
-    @Test
-    void testUniversalAdd_whenInvalidInputBody_returnBadRequest() throws Exception {
-        int num1 = 1, num2 = 101;
-        String body = """
-                        {
-                            "num1": %d,
-                            "num2": %d
-                        }
-                        """.formatted(num1, num2);
-
-        mockMvc.perform(post("/calculate/universalAdd")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .param("numeralSystem", "DEC")
-                        .content(body))
-                .andExpect(status().isBadRequest())
-                .andExpect(content().string(is(startsWith("Input validation failed"))))
-        ;
+                .andExpect(jsonPath("$.result", is(equalTo(num1 + num2))));
     }
 }
