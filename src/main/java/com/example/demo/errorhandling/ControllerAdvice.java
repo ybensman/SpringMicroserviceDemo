37450233c3919ccf3c9d377cc39397d4ee0b45ee
Bensman, Yulia <yb444b@intl.att.com>
2023-06-13 23:58:19 +0300

    Solutions for Configuration and Validation

--- src/main/java/com/example/demo/errorhandling/ControllerAdvice.java
+++ src/main/java/com/example/demo/errorhandling/ControllerAdvice.java
@@ -1,11 +1,16 @@
 package com.example.demo.errorhandling;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
@@ -21,6 +26,18 @@ public class ControllerAdvice extends ResponseEntityExceptionHandler {
     @ResponseStatus(BAD_REQUEST)
     public ResponseEntity<String> handleNumberFormatException(NumberFormatException ex) {
         return new ResponseEntity<>("Illegal number format exception", BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(BAD_REQUEST)
+    public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException ex) {
+        return new ResponseEntity<>("Input validation failed: " + ex.getMessage(), BAD_REQUEST);
+    }
+
+    @Override
+    @ResponseStatus(BAD_REQUEST)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity<>("Input validation failed", BAD_REQUEST);
     }
 
 }
--- src/main/java/com/example/demo/model/UniversalCalculateRequest.java
+++ src/main/java/com/example/demo/model/UniversalCalculateRequest.java
@@ -1,6 +1,18 @@
 package com.example.demo.model;
 
+import javax.validation.constraints.*;
+
 public record UniversalCalculateRequest(
-    String num1,
-    String num2) {
+        @NotNull
+        @NotBlank
+        @Min(1)
+        @Max(100)
+        @Pattern(regexp = "^[0-9]*[a-f]*$")
+        String num1,
+        @NotNull
+        @NotBlank
+        @Min(1)
+        @Max(100)
+        @Pattern(regexp = "^[0-9]*[a-f]*$")
+        String num2) {
 }
